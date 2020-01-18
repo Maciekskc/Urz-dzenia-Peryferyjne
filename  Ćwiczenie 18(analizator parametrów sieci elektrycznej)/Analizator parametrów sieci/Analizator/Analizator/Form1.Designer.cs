e5fca57ac063be80584bf244964389ed50e09dd5
maciekskc <241285@student.pwr.edu.pl>
2020-01-18 17:10:29 +0100

    upload lab 4

---
+++  Ćwiczenie 18(analizator parametrów sieci elektrycznej)/Analizator parametrów sieci/Analizator/Analizator/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Analizator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelVoltage = new System.Windows.Forms.Label();
+            this.textBoxVoltage = new System.Windows.Forms.TextBox();
+            this.textBoxCurrent = new System.Windows.Forms.TextBox();
+            this.labelCurrent = new System.Windows.Forms.Label();
+            this.textBoxCosinus = new System.Windows.Forms.TextBox();
+            this.labelCosinus = new System.Windows.Forms.Label();
+            this.textBoxActivePower = new System.Windows.Forms.TextBox();
+            this.labelPower = new System.Windows.Forms.Label();
+            this.timerUpdate = new System.Windows.Forms.Timer(this.components);
+            this.textBoxFrequency = new System.Windows.Forms.TextBox();
+            this.labelFrequency = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelVoltage
+            // 
+            this.labelVoltage.AutoSize = true;
+            this.labelVoltage.Location = new System.Drawing.Point(185, 45);
+            this.labelVoltage.Name = "labelVoltage";
+            this.labelVoltage.Size = new System.Drawing.Size(87, 20);
+            this.labelVoltage.TabIndex = 0;
+            this.labelVoltage.Text = "Voltage [V]";
+            // 
+            // textBoxVoltage
+            // 
+            this.textBoxVoltage.Location = new System.Drawing.Point(48, 68);
+            this.textBoxVoltage.Name = "textBoxVoltage";
+            this.textBoxVoltage.Size = new System.Drawing.Size(397, 26);
+            this.textBoxVoltage.TabIndex = 1;
+            // 
+            // textBoxCurrent
+            // 
+            this.textBoxCurrent.Location = new System.Drawing.Point(48, 149);
+            this.textBoxCurrent.Name = "textBoxCurrent";
+            this.textBoxCurrent.Size = new System.Drawing.Size(397, 26);
+            this.textBoxCurrent.TabIndex = 3;
+            // 
+            // labelCurrent
+            // 
+            this.labelCurrent.AutoSize = true;
+            this.labelCurrent.Location = new System.Drawing.Point(185, 126);
+            this.labelCurrent.Name = "labelCurrent";
+            this.labelCurrent.Size = new System.Drawing.Size(85, 20);
+            this.labelCurrent.TabIndex = 2;
+            this.labelCurrent.Text = "Current [A]";
+            // 
+            // textBoxCosinus
+            // 
+            this.textBoxCosinus.Location = new System.Drawing.Point(48, 228);
+            this.textBoxCosinus.Name = "textBoxCosinus";
+            this.textBoxCosinus.Size = new System.Drawing.Size(397, 26);
+            this.textBoxCosinus.TabIndex = 5;
+            // 
+            // labelCosinus
+            // 
+            this.labelCosinus.AutoSize = true;
+            this.labelCosinus.Location = new System.Drawing.Point(185, 205);
+            this.labelCosinus.Name = "labelCosinus";
+            this.labelCosinus.Size = new System.Drawing.Size(84, 20);
+            this.labelCosinus.TabIndex = 4;
+            this.labelCosinus.Text = "Cosinus(fi)";
+            // 
+            // textBoxActivePower
+            // 
+            this.textBoxActivePower.Location = new System.Drawing.Point(48, 304);
+            this.textBoxActivePower.Name = "textBoxActivePower";
+            this.textBoxActivePower.Size = new System.Drawing.Size(397, 26);
+            this.textBoxActivePower.TabIndex = 7;
+            // 
+            // labelPower
+            // 
+            this.labelPower.AutoSize = true;
+            this.labelPower.Location = new System.Drawing.Point(169, 281);
+            this.labelPower.Name = "labelPower";
+            this.labelPower.Size = new System.Drawing.Size(127, 20);
+            this.labelPower.TabIndex = 6;
+            this.labelPower.Text = "Active Power [W]";
+            // 
+            // timerUpdate
+            // 
+            this.timerUpdate.Interval = 500;
+            this.timerUpdate.Tick += new System.EventHandler(this.timerUpdate_Tick);
+            // 
+            // textBoxFrequency
+            // 
+            this.textBoxFrequency.Location = new System.Drawing.Point(48, 372);
+            this.textBoxFrequency.Name = "textBoxFrequency";
+            this.textBoxFrequency.Size = new System.Drawing.Size(397, 26);
+            this.textBoxFrequency.TabIndex = 9;
+            // 
+            // labelFrequency
+            // 
+            this.labelFrequency.AutoSize = true;
+            this.labelFrequency.Location = new System.Drawing.Point(169, 349);
+            this.labelFrequency.Name = "labelFrequency";
+            this.labelFrequency.Size = new System.Drawing.Size(108, 20);
+            this.labelFrequency.TabIndex = 8;
+            this.labelFrequency.Text = "Frequency Hz";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 432);
+            this.Controls.Add(this.textBoxFrequency);
+            this.Controls.Add(this.labelFrequency);
+            this.Controls.Add(this.textBoxActivePower);
+            this.Controls.Add(this.labelPower);
+            this.Controls.Add(this.textBoxCosinus);
+            this.Controls.Add(this.labelCosinus);
+            this.Controls.Add(this.textBoxCurrent);
+            this.Controls.Add(this.labelCurrent);
+            this.Controls.Add(this.textBoxVoltage);
+            this.Controls.Add(this.labelVoltage);
+            this.Name = "MainForm";
+            this.Text = "Analizator Parametrów Sieci";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelVoltage;
+        private System.Windows.Forms.TextBox textBoxVoltage;
+        private System.Windows.Forms.TextBox textBoxCurrent;
+        private System.Windows.Forms.Label labelCurrent;
+        private System.Windows.Forms.TextBox textBoxCosinus;
+        private System.Windows.Forms.Label labelCosinus;
+        private System.Windows.Forms.TextBox textBoxActivePower;
+        private System.Windows.Forms.Label labelPower;
+        private System.Windows.Forms.Timer timerUpdate;
+        private System.Windows.Forms.TextBox textBoxFrequency;
+        private System.Windows.Forms.Label labelFrequency;
+    }
+}
+
---
+++  Ćwiczenie 18(analizator parametrów sieci elektrycznej)/Analizator parametrów sieci/Analizator/Analizator/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Analizator
+{
+    public partial class MainForm : Form
+    {
+
+        private const string endPointIpv4Address = "169.254.2.10";
+        private const int endPointTCPPort = 502;
+        private TcpClient tcpClient;
+        private NetworkStream networkStream;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            tcpClient = new TcpClient();
+            try
+            {
+                tcpClient.Connect(IPAddress.Parse(endPointIpv4Address), endPointTCPPort);
+                networkStream = tcpClient.GetStream();
+                timerUpdate.Start();
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Environment.Exit(0);
+            }
+        }
+
+        private void timerUpdate_Tick(object sender, EventArgs e)
+        {
+            update();
+        }
+
+        private void update()
+        {
+            textBoxVoltage.Text = getVoltageL1();
+            textBoxCurrent.Text = getCurrentL1();
+            textBoxCosinus.Text = getCosPhi();
+            textBoxActivePower.Text = getActivePowerL1();
+            textBoxFrequency.Text = getFrequencyL1();
+        }
+
+        string getVoltageL1()
+        {
+            byte[] request =
+            {
+                //nr zapytania 2B
+                0x00, 0x01,
+                //protokol modbus
+                0x00, 0x00,
+                //dlugosc danych
+                0x00, 0x06,
+                //nr urzadzenia
+                0x01,
+                //funkcja read
+                0x03,
+                //addres rejestru
+                0x10, 0x02,
+                //liczba rejestrow
+                0x00, 0x02
+            };
+            networkStream.Write(request, 0, request.Length);
+
+            byte[] response = new byte[15];
+            networkStream.Read(response, 0, response.Length);
+            Console.WriteLine("Napiecie:\n" + BitConverter.ToString(response));
+
+            byte[] voltageL1Hex = new byte[4];
+            Array.ConstrainedCopy(response, 9, voltageL1Hex, 0, voltageL1Hex.Length);
+            Array.Reverse(voltageL1Hex);
+            return (BitConverter.ToUInt32(voltageL1Hex, 0) / 1000f).ToString();
+        }
+
+        string getCurrentL1()
+        {
+            byte[] request =
+            {
+                0x00, 0x01,
+                0x00, 0x00,
+                0x00, 0x06,
+                0x01,
+                0x03,
+                0x10, 0x10,
+                0x00, 0x02
+            };
+            networkStream.Write(request, 0, request.Length);
+
+            byte[] response = new byte[15];
+            networkStream.Read(response, 0, response.Length);
+            Console.WriteLine("Natezenie:\n" + BitConverter.ToString(response));
+
+            byte[] currentL1Hex = new byte[4];
+            Array.ConstrainedCopy(response, 9, currentL1Hex, 0, currentL1Hex.Length);
+            Array.Reverse(currentL1Hex);
+            return (BitConverter.ToUInt32(currentL1Hex, 0) / 1000f).ToString();
+        }
+        public string getCosPhi()
+        {
+            byte[] request =
+            {
+                0x00, 0x01,
+                0x00, 0x00,
+                0x00, 0x06,
+                0x01,
+                0x03,
+                0x10, 0x20,
+                0x00, 0x02
+            };
+            networkStream.Write(request, 0, request.Length);
+
+            byte[] response = new byte[15];
+            networkStream.Read(response, 0, response.Length);
+            Console.WriteLine("Cos fi\n" + BitConverter.ToString(response));
+
+
+            byte[] cosinusL1Hex = new byte[4];
+            Array.ConstrainedCopy(response, 9, cosinusL1Hex, 0, cosinusL1Hex.Length);
+            Array.Reverse(cosinusL1Hex);
+            return (BitConverter.ToInt32(cosinusL1Hex, 0) / 1000f).ToString();
+        }
+
+        public string getActivePowerL1()
+        {
+            byte[] request =
+            {
+                0x00, 0x01,
+                0x00, 0x00,
+                0x00, 0x06,
+                0x01,
+                0x03,
+                0x10, 0x30,
+                0x00, 0x02
+            };
+            networkStream.Write(request, 0, request.Length);
+
+            byte[] response = new byte[15];
+            networkStream.Read(response, 0, response.Length);
+            Console.WriteLine("Moc czynna:\n" + BitConverter.ToString(response));
+
+            byte[] activePowerL1Hex = new byte[4];
+            Array.ConstrainedCopy(response, 9, activePowerL1Hex, 0, activePowerL1Hex.Length);
+            Array.Reverse(activePowerL1Hex);
+            return (BitConverter.ToInt32(activePowerL1Hex, 0)).ToString();
+        }
+
+        public string getFrequencyL1()
+        {
+            byte[] request =
+            {
+                0x00, 0x01,
+                0x00, 0x00,
+                0x00, 0x06,
+                0x01,
+                0x03,
+                0x10, 0x74,
+                0x00, 0x02
+            };
+            networkStream.Write(request, 0, request.Length);
+
+            byte[] response = new byte[15];
+            networkStream.Read(response, 0, response.Length);
+            Console.WriteLine("Częstotliwość :\n" + BitConverter.ToString(response));
+
+            byte[] frequencyL1Hex = new byte[4];
+            Array.ConstrainedCopy(response, 9, frequencyL1Hex, 0, frequencyL1Hex.Length);
+            Array.Reverse(frequencyL1Hex);
+            return (BitConverter.ToInt32(frequencyL1Hex, 0)).ToString();
+        }
+
+    }
+}
